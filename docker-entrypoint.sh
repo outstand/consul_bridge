8c8d8de7b27b86ac69506fddba0dec234a133170
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-04-26 13:16:45 -0700

    Run service as root
    This is needed to run under system-docker on rancherOS.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,9 @@ if consul_bridge help "$1" 2>&1 | grep -q "consul_bridge $1"; then
   set -- consul_bridge "$@"
 fi
 
-if [ "$1" = 'consul_bridge' ]; then
-  set -- gosu bridge "$@"
-fi
+# Enable this to run as an unprivileged user
+# if [ "$1" = 'consul_bridge' ]; then
+#   set -- gosu bridge "$@"
+# fi
 
 exec "$@"
